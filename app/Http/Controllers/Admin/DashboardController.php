995f28965f12e5af185fd6b19d464ebb4c31ecb5
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2020-09-06 07:36:13 +0700

    add count data to admin panel

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Model\donation;
+use App\Model\cause;
+use App\User;
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard');
+        return view('pages.dashboard',[
+            'donation' => donation::latest()->get(),
+            'total_donate' => donation::count(),
+            'donate_totals' => donation::sum('total_donation'),
+            'causes' => cause::count(),
+            'user' => User::count(),
+        ]);
     }
 }
